e4cd36976b44dd72da3b1f8c22c875ee485c3feb
phạm tú tài <93572750+phamtutai1338@users.noreply.github.com>
2022-07-21 23:09:51 +0700

    Delete config.php

--- config.php
+++
@@ -1,8 +0,0 @@
-<?php
-$dataHost ='localhost';
-$dataName ='student_db' ;
-$dataUserName = 'root';
-$dataPassword ='';
-$mysqli= mysqli_connect($dataHost,$dataUserName,$dataPassword,$dataName);
-?>
-
